38cb9b33f30abcd6c8b78cc797e647c1c166bfd4
Leonid Kalintsev <lensuperhero@gmail.com>
2025-11-17 10:04:35 +0300

    ViewSet and Routers

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -16,12 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from women.views import WomenViewSet
+from django.urls import include
 
-from women.views import WomenApiList, WomenApiUpdate, WomenApiDetailView
+# class MyCustomRouter(routers.SimpleRouter):
+#     routes = [
+#         routers.Route(url=r'^{prefix}$',
+#                       mapping={'get': 'list'},
+#                       name='{basename}-list',
+#                       detail=False,
+#                       initkwargs={'suffix': 'List'}),
+#         routers.Route(url=r'^{prefix}/{lookup}$',
+#                       mapping={'get': 'retrieve'},
+#                       name='{basename}-detail',
+#                       detail=True,
+#                       initkwargs={'suffix': 'Detail'})
+#     ]
+
+
+router = routers.SimpleRouter()
+router.register(r'women', WomenViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomenApiList.as_view()),
-    path('api/v1/womenlist/<int:pk>/', WomenApiUpdate.as_view()),
-    path('api/v1/womendetail/<int:pk>/', WomenApiDetailView.as_view()),
+    path('api/v1/', include(router.urls)),
 ]
--- women/views.py
+++ women/views.py
@@ -1,20 +1,26 @@
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import generics
-from women.models import Women
+
+from women.models import *
 from women.serializers import WomenSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
 
-class WomenApiList(generics.ListCreateAPIView):
+class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
 
 
-class WomenApiUpdate(generics.UpdateAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cat = Category.objects.get(pk=pk)
+        return Response({'cats': cat.name})
 
 
-class WomenApiDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+
+        if pk is None:
+            return Women.objects.all()[:3]
+        else:
+            return Women.objects.filter(pk=pk)
