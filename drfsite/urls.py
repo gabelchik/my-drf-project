7f5f7f9ae807946a16f9941d0578788b96c29ae4
Leonid Kalintsev <lensuperhero@gmail.com>
2025-11-19 19:05:34 +0300

    permissions+

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from women.views import WomenViewSet
+from women.views import WomenAPIList, WomenAPIUpdate, WomenAPIDestroy
 from django.urls import include
 
 # class MyCustomRouter(routers.SimpleRouter):
@@ -35,11 +35,13 @@ from django.urls import include
 #     ]
 
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+# router = routers.SimpleRouter()
+# router.register(r'women', WomenViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
+    path('api/v1/women/', WomenAPIList.as_view()),
+    path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
+    path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
 ]
--- women/views.py
+++ women/views.py
@@ -1,26 +1,45 @@
-from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from women.permissions import *
 from women.models import *
 from women.serializers import WomenSerializer
+from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.decorators import action
 
 
-class WomenViewSet(viewsets.ModelViewSet):
+# class WomenViewSet(viewsets.ModelViewSet):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+#
+#
+#     @action(methods=['get'], detail=True)
+#     def category(self, request, pk=None):
+#         cat = Category.objects.get(pk=pk)
+#         return Response({'cats': cat.name})
+#
+#
+#     def get_queryset(self):
+#         pk = self.kwargs.get('pk')
+#
+#         if pk is None:
+#             return Women.objects.all()[:3]
+#         else:
+#             return Women.objects.filter(pk=pk)
+
+class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
-    @action(methods=['get'], detail=True)
-    def category(self, request, pk=None):
-        cat = Category.objects.get(pk=pk)
-        return Response({'cats': cat.name})
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
 
 
-    def get_queryset(self):
-        pk = self.kwargs.get('pk')
-
-        if pk is None:
-            return Women.objects.all()[:3]
-        else:
-            return Women.objects.filter(pk=pk)
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAdminOrReadOnly,)
